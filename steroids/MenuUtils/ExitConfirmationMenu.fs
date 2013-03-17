79d57c16ec57c926b2f17642418536291e5db327
Jack Jamieson <jamieson.jack@gmail.com>
2013-03-17 08:49:58 -0400

    the full thing - score works too...sort of
    adding is not precise

---
+++ steroids/MenuUtils/ExitConfirmationMenu.fs
@@ -0,0 +1,64 @@
+﻿module ExitConfirmationMenu
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+open Casanova
+open Casanova.Core
+open Casanova.Coroutines
+open Casanova.Utilities
+open Casanova.Math
+open Casanova.Game
+open Casanova.Drawing
+open Casanova.StandardLibrary
+open Casanova.StandardLibrary.Core
+
+// The Menu contains the text to draw and the associated layer.
+[<CasanovaEntity>]
+type Menu = {
+    // The layer used to draw the menu
+    Layer     : SpriteLayer
+    // The drawable text to be displayed
+    Text      : DrawableText
+  }
+
+
+let start_menu (args : StartGameArgs) = 
+  // The layer
+  let layer = SpriteLayer.Create(args.graphics_device, args.content)
+  // The menu is created with the layer and with the drawable text
+  let world = 
+    {
+      Layer = layer
+      Text = 
+        DrawableText.Create(
+          layer,
+          "Arial",
+          -Vector2<_>.UnitX * 490.f,
+          Vector2<_>.UnitY * 50.f,
+          "Quit?\nYes (Y)\nNo (N)",
+          Color.White,
+          Vector2<_>.One * 200.f)
+    }
+
+  // The (!) operator which in rules is used to access the current value, in scripts 
+  // will access the next value; this way scripts act as completely imperative
+  // coroutines
+  let inline (!) x = immediate_lookup x
+  
+  // The main script does nothing.
+  let main = 
+    co{ do! yield_} |> repeat_
+
+  // Three input scripts: 
+  // when the user presses the Y key the menu will invoke exit method that closes the application;
+  // when the user presses the N or Escape key the menu will be pop the stack removing itself from it;
+  let input = 
+    [
+      wait_key_press Keys.Y =>> co{ do args.exit() };
+      wait_key_press Keys.N =>> co{ do args.pop_stack() }
+      wait_key_press Keys.Escape =>> co{ do args.pop_stack() }
+    ]
+  world, main, input
+
+
---
+++ steroids/MenuUtils/MainMenu.fs
@@ -0,0 +1,72 @@
+﻿module MainMenu
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
+open Casanova
+open Casanova.Core
+open Casanova.Coroutines
+open Casanova.Utilities
+open Casanova.Math
+open Casanova.Game
+open Casanova.Drawing
+open Casanova.StandardLibrary
+open Casanova.StandardLibrary.Core
+
+// The Menu contains the menu content text, the background and the layer associated.
+[<CasanovaEntity>]
+type Menu = {
+    // The layer used to draw the background
+    BackgroundLayer : SpriteLayer
+    // The layer used to draw the menu
+    TextLayer     : SpriteLayer
+    // The background image of this menu
+    Background : DrawableSprite
+    // The drawable text with the content
+    Text      : DrawableText
+  }
+
+
+let start_menu game_name background start_actual_game (args : StartGameArgs) = 
+  // The layer for the text
+  let layer = SpriteLayer.Create(args.graphics_device, args.content)
+  // The layer for the background
+  let background_layer = SpriteLayer.Create(args.graphics_device, args.content)
+  // The menu is created with the layer and with the two drawable text
+  let world = 
+    {
+      TextLayer = layer
+      BackgroundLayer = background_layer
+      Background = 
+        DrawableSprite.Create(background_layer, Vector2<pixel>.Zero, Vector2<pixel>.One * 2000.0f, background)
+      Text = 
+        DrawableText.Create(
+          layer,
+          "Arial",
+          -Vector2<_>.UnitX * 350.f,
+          Vector2<_>.One * 50.f,
+          "Start new game (N)\nLoad (F10)\nQuit (Q)",
+          Color.White,
+          Vector2<_>.One * 150.f)
+    }
+
+  // The (!) operator which in rules is used to access the current value, in scripts 
+  // will access the next value; this way scripts act as completely imperative
+  // coroutines
+  let inline (!) x = immediate_lookup x
+  
+  // The main script does nothing.
+  let main = 
+    co{ do! yield_}
+
+  // Five input scripts: 
+  // when the user presses F10 key the game will be loaded
+  // when the user presses N key a new game wil start
+  // when the user presses Q key it quit
+  let input = 
+    [
+      wait_key_press Keys.F10 =>> args.load game_name
+      wait_key_press Keys.N =>> co{ do args.SetStack start_actual_game}
+      wait_key_press Keys.Q =>> co{ do args.exit() }
+    ]
+  world, main, input
